0b33c028d4485f4fa3382d6894d8a55281f68409
Aaron Mavrou <amavrou@uwo.ca>
2018-11-24 23:24:12 +0000

    modifications regarding my service files

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,12 +1,25 @@
 import { EventEmitter } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
     
     private recipes: Recipe[] = [
-    new Recipe('First Test Recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg'),
-    new Recipe('Second Test Recipe', 'This is simply a test', 'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg')
+    new Recipe('First Test Recipe', 
+    'This is simply a test', 
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+    [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+        ]),
+    new Recipe('Second Test Recipe',
+    'This is simply a test',
+    'https://get.pxhere.com/photo/dish-meal-food-produce-recipe-fish-breakfast-meat-pork-cuisine-steak-pork-chop-power-dishes-grilling-fried-food-604134.jpg',
+    [
+       new Ingredient('Buns', 2),
+       new Ingredient('Meat', 1)
+        ])
     ];
     
     getRecipes(){
